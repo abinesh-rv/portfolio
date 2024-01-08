f74059688e79b9bfafe53e38a53b0ba415dda643
Abinesh <abinesh.ravi24@gmail.com>
2024-01-08 19:40:25 +0530

    updated resume and optimized screen layout

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useRef} from 'react'
+import React, { useEffect, useRef } from 'react'
 import NavBar from "./components/navbar"
 import Hero from "./components/hero"
 import About from "./components/about"
@@ -8,50 +8,57 @@ import Contact from './components/contact'
 import AOS from "aos"
 import "aos/dist/aos.css";
 
-function App() {
+function App()
+{
 
-   useEffect(()=>{
-      AOS.init({duration : 2000});
-   },[])
+   useEffect(() =>
+   {
+      AOS.init({ duration: 2000 });
+   }, [])
 
-  const aboutRef  = useRef()
-  const skillsRef  = useRef()
-  const worksRef  = useRef()
-  const contactRef  = useRef()
-  const heroRef  = useRef()
+   const aboutRef = useRef()
+   const skillsRef = useRef()
+   const worksRef = useRef()
+   const contactRef = useRef()
+   const heroRef = useRef()
 
-  const handleAbout = () => {
-     aboutRef.current?.scrollIntoView({behavior:"smooth"})
-  }
+   const handleAbout = () =>
+   {
+      aboutRef.current?.scrollIntoView({ behavior: "smooth" })
+   }
 
-  const handleSkills = () => {
-     skillsRef.current?.scrollIntoView({behavior:"smooth"})
-  }
+   const handleSkills = () =>
+   {
+      skillsRef.current?.scrollIntoView({ behavior: "smooth" })
+   }
 
-  const handleWorks = () => {
-     worksRef.current?.scrollIntoView({behavior:"smooth"})
-  }
+   const handleWorks = () =>
+   {
+      worksRef.current?.scrollIntoView({ behavior: "smooth" })
+   }
 
-  const handleContact = () => {
-     contactRef.current?.scrollIntoView({behavior:"smooth"})
-  }
+   const handleContact = () =>
+   {
+      contactRef.current?.scrollIntoView({ behavior: "smooth" })
+   }
 
-  const handleHero = () => {
-     heroRef.current?.scrollIntoView({behavior:"smooth"})
-  }
+   const handleHero = () =>
+   {
+      heroRef.current?.scrollIntoView({ behavior: "smooth" })
+   }
 
-  return (
-    <div className='bg-black'>
-        <NavBar handleAbout={handleAbout} handleSkills={handleSkills} handleWorks={handleWorks} handleContact={handleContact} handleHero={handleHero}/>
-        <div className='mx-12 overflow-hidden'>
-        <div data-aos="fade-up"><Hero heroRef={heroRef}/></div>
-        <div data-aos="flip-up"><About aboutRef={aboutRef}/></div>
-        <div data-aos="zoom-in"><Skills skillsRef={skillsRef}/></div>
-        <div data-aos="zoom-in"><Works worksRef={worksRef}/></div>
-        <div data-aos="fade-up"><Contact contactRef={contactRef}/></div>
-        </div>
-    </div>
-  )
+   return (
+      <div className='bg-black max-w-[1400px] mx-auto'>
+         <NavBar handleAbout={handleAbout} handleSkills={handleSkills} handleWorks={handleWorks} handleContact={handleContact} handleHero={handleHero} />
+         <div className='mx-12 overflow-hidden'>
+            <div data-aos="fade-up"><Hero heroRef={heroRef} /></div>
+            <div data-aos="flip-up"><About aboutRef={aboutRef} /></div>
+            <div data-aos="zoom-in"><Skills skillsRef={skillsRef} /></div>
+            <div data-aos="zoom-in"><Works worksRef={worksRef} /></div>
+            <div data-aos="fade-up"><Contact contactRef={contactRef} /></div>
+         </div>
+      </div>
+   )
 }
 
 export default App
